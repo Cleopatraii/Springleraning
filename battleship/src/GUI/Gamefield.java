20d49dcebd1ccb104dffa5523b384d5ac8c3e440
Henning Koepchen <henning-walter.koepchen@stud.hs-hannover.de>
2022-11-08 15:21:30 +0100

    Draw Gamefieldraster Images importet

--- battleship/src/GUI/Gamefield.java
+++ battleship/src/GUI/Gamefield.java
@@ -2,8 +2,12 @@ package GUI;
 
 import java.awt.*;
 import java.awt.image.*;
+import java.io.IOException;
+
 public class Gamefield {
 
+
+    private boolean imagesscaled = false;
     private double windowx;
     private double windowy;
 
@@ -11,14 +15,29 @@ public class Gamefield {
 
     private int fieldy;
     private BufferedImage ship;
+    private  BufferedImage waterhit;
+    private  BufferedImage shiphit;
+    private  BufferedImage shipsunk;
+
     public Gamefield(double x, double y, Window window){
         this.windowx = x;
         this.windowy = y;
 
-        SpriteSheet ss = new SpriteSheet(window.getSpriteSheet());
-
-        ship = ss.grabImage(1,1,48,48);
+        BufferedImageLoader loader = new BufferedImageLoader();
+        try {
+            ship = loader.loadImage("/boat.png");
+            waterhit = loader.loadImage("/water1.png");
+            shiphit = loader.loadImage("/hit.png");
+            shipsunk = loader.loadImage("/sunk.png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void scaleimage(int size) {
+
+    }
+
     //update method
     public void tick() {
 
@@ -32,8 +51,27 @@ public class Gamefield {
         drawfield(25,25,450,450,field,g);
         drawfield(525,25,450,450,field,g);
     }
+
+    //Spielfeld wird gezeichnet
     public void drawfield(int xoffset, int yoffset, int xsize, int ysize, int[][] field, Graphics g) {
         int fieldsize = field.length;
+        if(imagesscaled== false) {
+            scaleimage(xsize/fieldsize);
+            imagesscaled = true;
+        }
+        for(int j = 0; j < fieldsize; j++){
+            for(int i = 0; i <fieldsize; i ++) {
+                switch (field[i][j]) {
+                    case 0: break; //Wasser
+                    case 1: //schiff
+
+                    case 2: //Wasser getroffen
+                    case 3: //Schiff getroffen
+                    case 4: //Schiff versenkt
+                    default: break;
+                }
+            }
+        }
         for (int i = 0; i < fieldsize; i++) {
             g.drawLine(xoffset + (xsize/fieldsize)*i,yoffset,xoffset+(xsize/fieldsize)*i,yoffset+ ysize);
             g.drawLine(xoffset,yoffset + (ysize/fieldsize)*i,xoffset+xsize,yoffset + (ysize/fieldsize)*i);
