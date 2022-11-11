3f06de0c7057ac52a464b71146761f92237afea6
fmf-qsu-u1 <suhaila.alhafez@stud.hs-hannover.de>
2022-11-11 13:10:54 +0100

    placeShip gefüllt

--- battleship/src/gameLogic/GameClassGamelogic.java
+++ battleship/src/gameLogic/GameClassGamelogic.java
@@ -5,6 +5,7 @@ package gameLogic;
  * Klasse als Verbindungsglied zum Programmteil Game-Logik
  */
 //TODO Kommentare überarbeiten >>GameLogik
+import game.Game;
 import game.Ship;
 
 import java.awt.*;
@@ -12,8 +13,27 @@ import java.awt.*;
 public class GameClassGamelogic {
     //Methode zur Überprüfung Regeln und Weitergabe in Shiparray
     public boolean placeShip(Ship ships){
-        //TODO Methode füllen >>GameLogik
-        return true; //TODO Return-Wert sinnvoll ersetzen >>GameLogik
+        //Deklaratio Variablen
+        Point[] position=ships.getPosition();
+        int[][] fieldarray= Game.getInstance().getPlayerfield();
+        int x = 0;
+        int y = 0;
+        //Regelüberprüfung
+        for (int i = 0;i<position.length; i++){     //Schleife für position
+            x = position[i].x;
+            y = position[i].y;
+            for(int fx = -1;fx<2;fx++){  //Schleife für x
+                for (int fy = -1;fy<2;fy++){     //Schleife für y
+                    if(fieldarray[y+fy][x+fx]==1) return false;
+                }
+            }
+        }
+        //Position an playerfield übergeben
+        for (int j = 0; j<position.length;j++){
+            Game.getInstance().setPlayerfield(position[j].y,position[j].x,1);
+        }
+
+        return true;
     }
     //Methode zu Trefferüberprüfung (return 0>nicht getroffen, 1>getroffen)
     public int opponentTurn(Point position){
